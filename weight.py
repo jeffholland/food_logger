bd46c0e3da7496955d84de9aadc9b07a48442944
Jeff Holland <holland.musictech@gmail.com>
2024-11-10 09:45:24 -0500

    can log non-whole numbers (decimals)

--- weight.py
+++ weight.py
@@ -5,7 +5,17 @@ from datetime import date
 con = sqlite3.connect('self.db')
 cur = con.cursor()
 
-if len(argv) == 2 and argv[1].isdigit():
+def validate_number(input):
+    if input.isdigit(): return True
+
+    split_decimal = input.split('.')
+    if len(split_decimal) == 2 and split_decimal[0].isdigit() and split_decimal[1].isdigit():
+        return True
+    
+    return False
+
+
+if len(argv) == 2 and validate_number(argv[1]):
     cur.execute(f"INSERT INTO weight VALUES('{date.today().isoformat()}', {argv[1]})")
     con.commit()
     print(f"Logged {argv[1]} pounds for today")
